82c478ce4e6485a1cfbc6fa96d92855200c5e01d
John Genere <johncgenere@gmail.com>
2019-06-14 12:06:26 -0400

    Initial File Setup

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Already Better than CunyFirst </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
