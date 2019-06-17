d981df5503b43442c89118e7f695918207b87607
John Genere <johncgenere@gmail.com>
2019-06-17 14:47:53 -0400

    Removed Schedule from Home Page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Calendar from './Calendar';
 import '../styles/App.css';
 
 const Home = () => {
@@ -7,7 +6,7 @@ const Home = () => {
     <div className="App">
       <header className="App-header">
         <h1 style={{color: 'black'}}> Already Better than CunyFirst </h1>
-        <Calendar />
+
       </header>
     </div>
   );
