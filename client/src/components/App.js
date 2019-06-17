54fef00b2771f8f6acdfc38721e8937c8784e4df
Jessica Wong <wong.jessica0411@gmail.com>
2019-06-17 10:38:05 -0400

    this should be decent

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
+import Login from './Login';
 import '../styles/App.css';
 
 class App extends Component {
@@ -13,6 +14,11 @@ class App extends Component {
               return (<Home />);
             }
           }/>
+          <Route path ="/login" exact render = {
+            () => {
+              return (<Login />);
+            }
+          }/>          
         </div>
       </Router>
     );
---
+++ client/src/components/Login.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import '../styles/login.css';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+
+    // <div className="title">
+
+    //     <h1> CUNYFirst Login Page </h1>
+
+    // </div>
+
+    <div className="App">
+
+<div id="id01" class="modal">
+  
+    <div class="container">
+
+    <button className="ui button"><Link to='/'>Home</Link></button>
+    <h2> Log in with your CUNY Login credentials(CUNYfirst Username@login.cuny.edu and CUNYfirst Password) </h2>
+
+    <h3> Please input your CUNY email</h3>
+      <div className="ui input error">
+            <input type="text" placeholder="CUNY email"></input>
+        </div>        <br></br>
+    <h4> Please enter your password </h4>
+      <div className="ui input">
+      <input type="password" placeholder="Enter Password" name="psw" required></input>
+      </div>
+        <br></br>
+        <br></br>
+    <button class="ui button" type="submit">
+      Login
+    </button>
+      <br></br>
+    <span class="psw"><br></br> <a href="#">Forgot your password?</a></span>
+    </div>
+
+</div>
+
+    <header className="pic">
+        <img src="https://ssologin.cuny.edu/images/cuny-logo.jpg"></img>
+    </header>
+
+
+      <header className="App-title">
+
+        <h1> CUNYFirst Login Page </h1>
+
+      </header>
+
+      <header className="App-header">
+
+      </header>
+    </div>
+
+  );
+}
+
+export default Login;
