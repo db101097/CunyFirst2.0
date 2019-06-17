2cd688e1bfe2f8a8f11111f3c9c674be9692c670
John Genere <johncgenere@gmail.com>
2019-06-17 13:03:10 -0400

    Added Register Page with Functionality

---
+++ client/src/components/Register.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Register extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      major: ""
+    }
+
+    this.handleFirstNameInput = this.handleFirstNameInput.bind(this);
+    this.handleLastNameInput = this.handleLastNameInput.bind(this);
+    this.handleEmailInput = this.handleEmailInput.bind(this);
+    this.handlePasswordInput = this.handlePasswordInput.bind(this);
+    this.handlePasswordConfirm = this.handlePasswordConfirm.bind(this);
+    this.handleMajorInput = this.handleMajorInput.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+
+    axios.defaults.withCredentials = true
+  }
+
+  handleFirstNameInput = (e) => {
+     this.setState({firstName: e.target.value});
+  }
+
+  handleLastNameInput = (e) => {
+     this.setState({lastName: e.target.value});
+  }
+
+  handleEmailInput = (e) => {
+    this.setState({email: e.target.value});
+  }
+
+  handlePasswordInput = (e) => {
+    this.setState({password: e.target.value});
+  }
+
+  handlePasswordConfirm = (e) => {
+    this.setState({passwordConfirm: e.target.value});
+  }
+
+  handleMajorInput = (e) => {
+    this.setState({major: e.target.value});
+  }
+
+  handleHomeClick = (event) => {
+    console.log('Go home!');
+    window.location.replace('/');
+  }
+
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      console.log(this.state);
+
+      // api call done here
+    }
+  }
+
+  handleSignUp = () => {
+    console.log(this.state);
+
+    // api call done here
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <div className="App-header">
+          <div style={{width: '25%'}}>
+            <div className="ui middle aligned center aligned grid">
+              <div className="column">
+                <h2 className="ui black image header">
+                  <div className="content">
+                    Register Your Account
+                  </div>
+                </h2>
+                <form className="ui large form" onKeyPress={this.handleKeyPress}>
+                  <div className="ui stacked segment">
+                    <div className="field">
+                      <div className="ui left icon input">
+                        <i className="user icon"></i>
+                        <input
+                          type="text"
+                          name="firstName"
+                          placeholder="First Name"
+                          onChange={this.handleFirstNameInput}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="ui left icon input">
+                        <i className="user icon"></i>
+                        <input
+                          type="text"
+                          name="lastName"
+                          placeholder="Last Name"
+                          onChange={this.handleLastNameInput}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="ui left icon input">
+                        <i className="at icon"></i>
+                        <input
+                          type="text"
+                          name="email"
+                          placeholder="Email"
+                          onChange={this.handleEmailInput}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="ui left icon input">
+                        <i className="lock icon"></i>
+                        <input
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                          onChange={this.handlePasswordInput}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="ui left icon input">
+                        <i className="lock icon"></i>
+                        <input
+                          type="password"
+                          name="passwordConfirm"
+                          placeholder="Confirm Password"
+                          onChange={this.handlePasswordConfirm}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="ui left icon input">
+                        <i className="book icon"></i>
+                        <input
+                          type="text"
+                          name="major"
+                          placeholder="Major"
+                          onChange={this.handleMajorInput}
+                        />
+                      </div>
+                    </div>
+                    <button className="ui fluid large black submit button" onClick={this.handleSignUp}>Register</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="ui fluid large black submit button" onClick={this.handleHomeClick}>Go Back Home</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
