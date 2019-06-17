09c82f9c0f0c3c1429078535bbaa1f9f933c85c3
johncgenere <johncgenere@gmail.com>
2019-06-17 13:27:26 -0400

    Created Register Page (#5)
    * Added Register Page with Functionality
    
    * Added more styling to Register Page
    
    * Removed Duplicate Folders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
+import Register from './Register';
 import '../styles/App.css';
 
 class App extends Component {
@@ -11,6 +12,11 @@ class App extends Component {
           <Route path ="/" exact render = {
             () => {
               return (<Home />);
+            }
+          }/>
+          <Route path ="/register" exact render = {
+            () => {
+              return (<Register />);
             }
           }/>
         </div>
