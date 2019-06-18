ee6f395b9c541a38cf7eaf106c78e1c421a8a665
Topgyal Gurung <topgyaltsering3@gmail.com>
2019-06-18 13:13:09 -0400

    search api

---
+++ server/models/migrations/20190618162334-create-classes.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Classes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Classes');
+  }
+};
