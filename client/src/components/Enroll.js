0f3ee642c6ffced72c86c7efe0a01e5d08823567
John Genere <johncgenere@gmail.com>
2019-06-19 11:16:09 -0400

    Search is functional now, local state saves search value

--- client/src/components/Enroll.js
+++ client/src/components/Enroll.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 
 class Enroll extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      searchedValue: ''
+    }
+
+    this.handleSearchInput = this.handleSearchInput.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  handleSearchInput = (e) => {
+    this.setState({searchedValue: e.target.value});
+  }
+
+  onKeyPress = (event) => {
+    if(event.key === 'Enter')
+      console.log(this.state.searchedValue);
+  }
+
+  onSubmit = (event) => {
+    console.log(this.state.searchedValue);
+  }
+
   render(){
     return (
       <div className="App">
         <div className="App-header">
           <h1> Search for a class </h1>
           <div className="ui icon input">
-            <input type="text" placeholder="Search..." />
-            <i className="search icon" />
+            <input
+              type="text"
+              placeholder="Search..."
+              onChange={this.handleSearchInput}
+              onKeyPress={this.onKeyPress}
+            />
+            <i className="inverted circular search link icon" onClick={this.onSubmit} />
           </div>
           <h1> Search for a class by subject </h1>
         </div>
