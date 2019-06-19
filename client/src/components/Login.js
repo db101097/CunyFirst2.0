b19317d886f31a9f4613f5af77dba7cec1ca1df0
John Genere <johncgenere@gmail.com>
2019-06-19 15:22:22 -0400

    Removed warnings

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { loginThunk } from '../thunks';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
