570dc8b6ebee074681af80502df84da6c2814c9a
Topgyal Gurung <topgyaltsering3@gmail.com>
2019-06-20 14:32:16 -0400

    closed issue #18

--- server/routes/search.js
+++ server/routes/search.js
@@ -4,7 +4,7 @@ module.exports=function(app,Class,meetInfo,schedule,Student){
 
     //query the model
     // first params(/:term) to be term searching for (eg:CSCI) AND second params to be student id
-    app.get('/:term/:studentId',async function(req,res){
+    app.get('/findConflict/:term/:studentId',async function(req,res){
         try{ 
             let createSearch= await search.up(Class)
             let result= await Class.sequelize.query('select distinct search.title, search.days,search."startTime", search."endTime" from (SELECT * FROM classes inner join "meetInfos" on classes.id="meetInfos"."classId" WHERE  _search @@ plainto_tsquery((:l), (:t))) as search inner join (select schedules."classId","studentId",subject,title,"courseNumber",section,name,days,"startTime","endTime",instructor,room from schedules inner join classes on classes.id=schedules."classId" inner join "meetInfos" on classes.id="meetInfos"."classId" where schedules."studentId"=(:sid)) as schedule on CAST(search."startTime" as TIME) < CAST(schedule."endTime" as TIME) and CAST(search."endTime" as TIME) > CAST(schedule."startTime" as TIME) and schedule.days && search.days and schedule."classId"!=search."classId"',
