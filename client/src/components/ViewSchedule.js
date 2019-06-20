d19269e52114799f9d88ec1ee6a93b56bce7f3a0
Topgyal Gurung <topgyaltsering3@gmail.com>
2019-06-20 19:26:45 -0400

    added exportpdf routes

--- client/src/components/ViewSchedule.js
+++ client/src/components/ViewSchedule.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import Calendar from './Calendar';
 import logo from '../images/cfirst.gif';
+import html2canvas from 'html2canvas';
+import axios from 'axios';
 
 class ViewSchedule extends Component {
   onLogout = (event) => {
     localStorage.clear();
     window.location.replace('/');
   }
+
+printDocument() {
+   const input = document.getElementById('sus');
+   html2canvas(input)
+     .then((canvas) => {
+       const imgData = canvas.toDataURL('image/png');
+        axios.post('http://localhost:8080/exportSchedule', {
+                img:imgData
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      // const pdf = new jsPDF();
+       //pdf.addImage(imgData, 'JPEG', 0, 0);
+       //pdf.output('dataurlnewwindow');
+       //pdf.save("download.pdf");
+     })
+   ;
+ }
 
   render(){
     return(
@@ -19,10 +43,11 @@ class ViewSchedule extends Component {
             <a className="ui item" style={{marginTop: '-20%', color: 'white', fontSize: '17px'}} onClick={this.onLogout} href='/'>
               Logout
             </a>
+            <button type="button" onClick={this.printDocument}>PDF</button>
           </div>
         </div>
         <div className='App'>
-          <div className="App-header">
+          <div id='sus' className="App-header">
             <Calendar />
           </div>
         </div>
