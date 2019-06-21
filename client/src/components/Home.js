22afac15c916d89b0194f8501d0d86d683ee8fe9
John Genere <johncgenere@gmail.com>
2019-06-21 00:19:46 -0400

    Home now has new Logo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/cfirst.gif';
+import logo from '../images/CSt.png';
 import '../styles/App.css';
 
 const Home = () => {
@@ -15,11 +15,11 @@ const Home = () => {
     <div className="App">
       <header className="App-header">
         <div style={{flexDirection: 'row', width: '100%'}}>
-          <h1 style={{color: 'black', fontSize: '275%'}}> Welcome to </h1>
-          <img src={logo} alt="CUNYFirst" style={{width: '17.5%', marginTop: '-1%', marginBottom: '-1.5%'}}/>
+          <h1 style={{color: 'black', fontSize: '275%', marginBottom: '-1%'}}> Welcome to </h1>
+          <img src={logo} alt="CUNYSecond" style={{width: '32.5%', marginTop: '0%', marginBottom: '0%'}}/>
         </div>
-        <div className="ui fluid large submit button" onClick={onLogin} style={{ backgroundColor: '#FD8317', width: '30%', marginBottom: '1%' }}> Login </div>
-        <div className="ui fluid large submit button" onClick={onRegister} style={{ backgroundColor: '#FD8317', width: '30%' }}> Register </div>
+        <div className="ui fluid large submit button" onClick={onLogin} style={{ backgroundColor: '#FDDD6E', width: '30%', marginBottom: '1%' }}> Login </div>
+        <div className="ui fluid large submit button" onClick={onRegister} style={{ backgroundColor: '#FDDD6E', width: '30%' }}> Register </div>
       </header>
     </div>
   );
